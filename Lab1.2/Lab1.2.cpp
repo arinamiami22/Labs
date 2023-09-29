0dcc713caf73582209ffb12b7f07fcf56d710174
Asus <Asus@ARINA>
2023-09-29 03:16:23 +0300

    Добавьте файлы проекта.

---
+++ Lab1.2/Lab1.2.cpp
@@ -0,0 +1,40 @@
+﻿// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    const int rows = 3;
+    const int cols = 3;
+    float arr[rows][cols];
+
+    std::cout << "Введите элементы массива 3x3:" << std::endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            std::cin >> arr[i][j];
+        }
+    }
+    float oneDimArr[rows * cols];
+    int index = 0;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            oneDimArr[index++] = arr[i][j];
+
+        }
+        std::sort(oneDimArr, oneDimArr + (rows * cols));
+
+        index = 0;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                arr[i][j] = oneDimArr[index++];
+            }
+        }
+        std::cout << "Отсортированный массив 3x3:" << std::endl;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                std::cout << arr[i][j] << "";
+            }
+            std::cout << std::endl;
+        }
+        return 0;
+    }
+}
---
+++ Labs/Lab1.1.cpp
@@ -0,0 +1,27 @@
+﻿// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    const int rows = 5;
+    const int cols = 5;
+    int arr[rows][cols];
+
+    std::cout << "Введите элементы массива 5x5:" << std::endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            std::cin >> arr[i][j];
+
+        }
+    }
+    int minElement = arr[0][0];
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (arr[i][j] < minElement) {
+                minElement = arr[i][j];
+            }
+        }
+    }
+    std::cout << "Минимальный элемент в массиве:" << std::endl;
+
+    return 0;
+}
